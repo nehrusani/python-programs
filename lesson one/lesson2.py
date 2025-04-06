d8f2acec702751aa14f0e2311c1b097280572d85
nehrusani@gmail.com <nehrusani@gmail.com>
2025-04-06 12:23:54 +0200

    Demo

--- lesson one/lesson2.py
+++ lesson one/lesson2.py
@@ -3,3 +3,7 @@ print("i like to play roblox")
 print("the result of 3+5 is ",3+5)
 Name="Ditya"
 print("my name is",Name) 
+print("the result of 8090+7859 total = ",8090+7859)
+Name="abc"
+print(Name)
+print(1+3)
---
+++ lesson one/lesson3.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+
+AccessKey='Enter your Accesskey here'
+SecretKey='Enter your Secretkey here'
+
+
+"""    Program to get instance running in any region """
+"""  it will help to not required to get any region statically """
+
+ec2 = boto3.Session(region_name="us-east-1",aws_access_key_id=AccessKey,aws_secret_access_key=SecretKey)
+client = ec2.client(service_name='ec2')
+all_regions = client.describe_regions()
+
+list_of_all_regions=[]
+for nehru in all_regions["Regions"]:
+    list_of_all_regions.append(nehru["RegionName"])
+ 
+for each_region in  list_of_all_regions:
+    ec2_instance=boto3.resource('ec2',each_region,aws_access_key_id=AccessKey,aws_secret_access_key=SecretKey)
+    for ec2ins in ec2_instance.instances.all():
+       for nehru in ec2ins.tags:
+           if nehru["Key"] == "Name":
