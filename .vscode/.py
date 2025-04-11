88cd143ba3734fa9d84adc6d7a36d50d0aeafb85
nehrusani@gmail.com <nehrusani@gmail.com>
2025-04-11 18:21:58 +0200

    demo

---
+++ .vscode/.py
@@ -0,0 +1,21 @@
+import os
+import random
+import turtle 
+turtle.fd(0)
+turtle.speed(0)
+turtle.bgcolor("black")
+
+turtle.ht()
+turtle.setundobuffer(1)
+turtle.tracer(1)
+class sprite(turtle.Turtle):
+  def _init_(self,spriteshape,color,startx,starty) :
+    turtle.Tuetle._init_(self,shpe = spriteshape)
+    self.speed(0)
+    self.penup()
+    self.color(color)
+    self.fd(0)
+    self.goto(startx,starty)
+    self.speed = 0
+
+delay =input("press enter to finsh. >")
---
+++ lesson3/lesson3.py
@@ -0,0 +1,10 @@
+print("welcome to condingal")
+print(3+8)
+name="LILY"
+print(name)
+x=5
+y=6
+print(x)
+print(y)
+import keyword
+print(keyword.kwlist)
