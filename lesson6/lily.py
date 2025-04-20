f6f1af38de675520b1a01d72b2910adb5ba545cc
nehrusani@gmail.com <nehrusani@gmail.com>
2025-04-20 13:22:17 +0200

    demo

---
+++ lesson6/lily.py
@@ -0,0 +1,8 @@
+username = input("please enter a username :")
+# print(username)
+total = int(username)
+if total / 2 == 0:
+    print("even")
+else:
+    print("odd")
+
---
+++ lesson7/logical operater 1.py
@@ -0,0 +1,3 @@
+a = int(input("enter a user:"))
+if a % 2 != 0:
+  print(a,"a is not even number")
