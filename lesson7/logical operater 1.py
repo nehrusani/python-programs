060368b6f09b6886ddb70d1d6a5399277f5e40cd
nehrusani@gmail.com <nehrusani@gmail.com>
2025-04-26 13:30:54 +0300

    demo

--- lesson7/logical operater 1.py
+++
@@ -1,3 +0,0 @@
-a = int(input("enter a user:"))
-if a % 2 != 0:
-  print(a,"a is not even number")
---
+++ lesson8/operraters3.py
@@ -0,0 +1,11 @@
+x=5
+if (type(x) is int):
+    print("x is an integer")
+else:
+    print("x is not an integer")
+    
+x=1
+if (type(x)is not float):
+    print("true")
+else:
+    print("false")
