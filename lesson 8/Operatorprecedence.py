a7179719741ab895db71cb632e7ed71713d8d438
nehrusani@gmail.com <nehrusani@gmail.com>
2025-05-11 13:47:17 +0300

    it is for dividing the number in oder

---
+++ lesson 8/Operatorprecedence.py
@@ -0,0 +1,14 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w)* x / y ;
+print("Value of (v + w)* x / y is",z)
+
+name = "Ayliva"
+age = 0
+if name == "Ayliva" or name == "Mini"and age >= 2 :
+ print("hello welcome")
+else:
+ print("good bye")
