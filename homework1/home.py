60ad8f582d1c1dd7ea02de667857920019817b46
nehrusani@gmail.com <nehrusani@gmail.com>
2025-05-17 15:53:11 +0300

    nested if

---
+++ homework1/home.py
@@ -0,0 +1,5 @@
+age = int(input("Enter : ")) 
+if age<=20 :
+  print("you can enter")
+else:
+   print("do not come")
---
+++ homework1/home2.py
@@ -0,0 +1,9 @@
+car = 2
+truk= 3
+cycle = 6
+if car == 2:
+    print(car)
+elif truk == 3:
+    print(truk)
+else:
+    print("NO")
