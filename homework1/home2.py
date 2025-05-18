e570a7b5fed521f2a325a39a06657580fdfe98e2
nehrusani@gmail.com <nehrusani@gmail.com>
2025-05-18 13:32:56 +0300

    print("i have used if statment!")

--- homework1/home2.py
+++ homework1/home2.py
@@ -1,8 +1,24 @@
-car = 2
-truk= 3
-if car == 2:
-    print(car)
-elif truk == 3:
-    print(truk)
+print("select your ride")
+print("1.car")
+print("2.bike")
+choice = input("enter your choice : ")
+if choice == "car":
+    print("pleas select the type of car")
+    print("1.Honda")
+    print("2.Mercedenc benz")
+    choice2 = input("enter your chioce : ")
+    if choice2 == "Honda":
+        print("you haveselected Honda")
+    else:
+        print("Mercedenz Benz")
+elif choice == "Bike" :
+    print("select the type of bike")
+    print("1.Track")
+    print("2.Haro") 
+    choice3 = input("enter your choice : ")
+    if choice3 == "Track" :
+        print("you have selected Track")
+    else:
+        print("you have seleted Haro")
 else:
-    print("NO")
+    print("wrong input!")        
