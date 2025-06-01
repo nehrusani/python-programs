37dc6e01712575fddebcd8aa21b0292d6f77ebc2
nehrusani@gmail.com <nehrusani@gmail.com>
2025-06-01 14:48:05 +0300

    we made a square

---
+++ square turtle/square.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().setup(580,600)
+turtle.Screen().bgcolor("green")
+polygon = turtle.Turtle()
+sides = 4
+angle = 360/sides
+for i in range(sides):
+    polygon.forward(80)
+    polygon.right(angle)
+
+turtle.done()
