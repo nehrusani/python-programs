65aebb266455fe52f9a5988f58c4984b45424e5c
nehrusani@gmail.com <nehrusani@gmail.com>
2025-06-01 13:13:48 +0300

    We made a small rightangletriangle there you can type a number and it will that many rows.

--- New folder (2)/mirroedtraingle.py
+++ New folder (2)/mirroedtraingle.py
@@ -1,7 +1,7 @@
 totalrows=int(input("Enter the Number of Rows : "))
-for row in range(1,totalrows+1):
-    for space in range(1,(totalrows-row)+ 1):
+for row in range(totalrows):
+    for space in range(totalrows-row-1):
         print(" ",end="")
-        for symbol in range(1,row+1):
-            print(" *",end=" ")
+    for symbol in range(row+1):
+            print("*",end="")
     print()
