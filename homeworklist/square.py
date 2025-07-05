4f27f0f8c172ab6d4969e636a78867ea2141bcf4
nehrusani@gmail.com <nehrusani@gmail.com>
2025-07-05 17:54:54 +0300

    f

---
+++ homeworklist/square.py
@@ -0,0 +1,3 @@
+mylist =  [2,3,5,6,6]
+mylist.reverse()
+print(mylist)  # This will print the reversed list: [6, 6, 5, 3, 2]
--- list/list.py
+++ list/list.py
@@ -2,7 +2,8 @@ mylist = []
 print(mylist,"empty list")
 mylist_2 = ["apple",1,"banana",2]
 print(mylist_2)
-mylist_3 = [1,2,3,6,8]*3
-print(mylist_3)
+mylist_3 = [1,2,3,6,8]
+del mylist_3[3]
+print( mylist_3) 
 mylist_2.reverse()
 print("reverse a list",mylist_2)
