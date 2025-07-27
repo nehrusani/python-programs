11cbe0cfef27113cb6e1d12f89af81b7add75046
nehrusani@gmail.com <nehrusani@gmail.com>
2025-07-27 21:10:11 +0300

    trh

---
+++ module/hello.py
@@ -0,0 +1,3 @@
+class Hello:
+    def say_hello(self, Name):
+        return f"Hello, {Name}!"
---
+++ module/tup.py
@@ -0,0 +1,4 @@
+from hello import Hello  # Import the Hello class
+greeter = Hello()
+message = greeter.Hello("Alice")
+print(message)
