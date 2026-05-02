ebf50d5507995678b4e38d08e7482457546ec74f
nehrusani@gmail.com <nehrusani@gmail.com>
2026-05-02 22:17:05 +0300

    kjiew

---
+++ killer+89/killer.py
@@ -0,0 +1,25 @@
+import time
+
+def sleep():
+    time.sleep(2.0)
+
+print("you are a new police officer")
+sleep()
+print("you are super happy")
+sleep()
+print("you are chilling on your desk eating donuts")
+sleep()
+print("but suddenly")
+sleep()
+print("a person ask you for help")
+sleep()
+print("the mafia gang want to kill me please help me")
+sleep()
+print("you think for a while ...")
+u = str(input("you : "))
+if u == "yes":
+    print("thankyou")
+    print("that person brought 4 people and asked: What were you doing at 9 clock")
+    print("statements:" \
+    "\n 1. i was in the shopping mall i did not blackmail him" \
+    "\n 2. i was dancing in my show why did you bring me here?")
