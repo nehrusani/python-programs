e8e10ccb1b6669471d0779d8464299081d7b2cf8
nehrusani@gmail.com <nehrusani@gmail.com>
2026-07-21 12:51:16 +0300

    zs5uzhsr9p

---
+++ bhanu bhaiya homeworks/home_work_21_july_2026.py
@@ -0,0 +1,35 @@
+# code one as you said it adds up first 10 numbers
+"""num = 1
+num1 = 2
+num2 = 3 
+num3 = 4
+num4 = 5
+num6 = 6
+num7 = 7
+num8 = 8
+num9 = 9
+num10 = 10
+print(num + num1 + num2+ num3+ num4+ num6+ num7+ num8+ num9+ num10)"""
+
+# code two as you said it swaps variable
+"""a = 10
+b = 2
+print('before swap:', a, b)
+temp = a
+a = b
+b = temp
+print('after swap:', a, b)"""
+
+#code three as you said it checks vowels
+"""tell = input("enter a word : ")
+vowels = "aeiouAEIOU"
+found = [ch for ch in tell if ch in vowels]
+if found:
+    print("Vowels found:", " ".join(found))
+else:
+    print("No vowels found")"""
+
+#code four as you said this pattern
+"""for i in range(1, 4):
+    print("* " * (i * 3))"""
+
